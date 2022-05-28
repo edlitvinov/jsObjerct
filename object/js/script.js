b95028d99730494258c4369ab7580d0ef49b3a96
edlitvinov <edoss@list.ru>
2022-05-28 21:31:32 +0400

    errors

--- object/js/script.js
+++ object/js/script.js
@@ -1,26 +1,49 @@
 'use strict';
 
+// errors
+
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error ('не переданы числа для сложения');
+    }
+    return a + b;
+}
+
+try {
+    sum();
+} catch(err) {
+    console.error(err);
+}
+
+console.log('The END.');
+
 // Object
 
-const dev = {
-    nane: 'John',
-    surname: 'Simpson',
-    age: 30,
-    skills: ['JS', 'HTML', 'CSS'],
-    isMaried: false,
-    addAge() {
-        this.age++;
-        console.log(this);  // this === объекту dev
-    },
-    getMaried() {
-        this.isMaried = true;
-    },
-    getDivorced() {
-        this.isMaried = false;
-    }
-};
+// push, pop - добавляет/удаляет в конец массива
+// shift, unshift - добавляет/удаляет вначало
 
-dev.addAge();
+// const num = [1, 2, 3, 4, 5];
+// console.log(num.includes(3));
+
+// const dev = {
+//     nane: 'John',
+//     surname: 'Simpson',
+//     age: 30,
+//     skills: ['JS', 'HTML', 'CSS'],
+//     isMaried: false,
+//     addAge() {
+//         this.age++;
+//         console.log(this);  // this === объекту dev
+//     },
+//     getMaried() {
+//         this.isMaried = true;
+//     },
+//     getDivorced() {
+//         this.isMaried = false;
+//     }
+// };
+
+// dev.addAge();
 
 // for (let prop in dev) {
 //     console.log(prop, dev[prop]);
